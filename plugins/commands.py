f44c12e72e305fb3a21b595e98c19bb17458fbee
yba9285 <108516941+yba9285@users.noreply.github.com>
2022-08-09 14:51:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/onlymoviies')
+                InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/marathimoviies')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/onlymoviies')
+            InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/marathimoviies')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/onlymoviies')
+            InlineKeyboardButton('MAIN_CHANNEL', url='https://t.me/marathimoviies')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
